a9880add218388b89ed6108ff06610fe3462b709
SzymonGier <73241031+SzymonGier@users.noreply.github.com>
2020-11-17 08:27:59 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args)  {
+
+        EventQueue.invokeLater( () ->{
+            new LogFrame();
+        });
+    }
+}
